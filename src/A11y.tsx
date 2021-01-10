e0d1b067bd11c70004da3a8fc3c818cae2d49a18
AlaricBaraou <alaric.baraou@gmail.com>
2021-01-10 22:24:17 +0100

    updated drei import path

--- src/A11y.tsx
+++ src/A11y.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState, useContext } from 'react';
 import { useThree } from 'react-three-fiber';
-import { Html } from '@react-three/drei';
+import { Html } from '@react-three/drei/Html';
 import useAnnounceStore from './announceStore';
 
 interface Props {
