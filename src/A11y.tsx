45f54926b58544f02df295db8a85453f8ade36d6
AlaricBaraou <alaric.baraou@gmail.com>
2021-03-13 13:18:53 +0100

    size fix

--- src/A11y.tsx
+++ src/A11y.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState, useContext } from 'react';
 import { useThree } from 'react-three-fiber';
-import { Html } from '@react-three/drei';
+import { Html } from '@react-three/drei/web/Html';
 import useAnnounceStore from './announceStore';
 import { useA11ySectionContext } from './A11ySection';
 
