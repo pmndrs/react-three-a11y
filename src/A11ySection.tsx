7a2fa083420d64e6df8164b231b6137aaa3602da
AlaricBaraou <alaric.baraou@gmail.com>
2021-04-01 16:29:13 +0200

    fix double section

--- src/A11ySection.tsx
+++ src/A11ySection.tsx
@@ -25,15 +25,24 @@ export const A11ySection: React.FC<Props> = ({
   const ref = useRef(null);
   const refpDesc = useRef(null);
   const gl = useThree(state => state.gl);
-  const [el] = React.useState(() => document.createElement('section'));
   const target = gl.domElement.parentNode;
 
   useEffect(() => {
+    if (ref.current === null) {
+      const el = document.createElement('section');
+      //@ts-ignore
+      target.appendChild(el);
+      //@ts-ignore
+      ref.current = el;
+    }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
     if (label) {
-      el.setAttribute('aria-label', label);
+      //@ts-ignore
+      ref.current.setAttribute('aria-label', label);
     }
-    el.setAttribute('r3f-a11y', 'true');
+    //@ts-ignore
+    ref.current.setAttribute('r3f-a11y', 'true');
     if (description) {
       if (refpDesc.current === null) {
         const pDesc = document.createElement('p');
@@ -41,7 +50,8 @@ export const A11ySection: React.FC<Props> = ({
         //sr-only
         pDesc.style.cssText =
           'border: 0!important;clip: rect(1px,1px,1px,1px)!important;-webkit-clip-path: inset(50%)!important;clip-path: inset(50%)!important;height: 1px!important;margin: -1px!important;overflow: hidden!important;padding: 0!important;position: absolute!important;width: 1px!important;white-space: nowrap!important;';
-        el.prepend(pDesc);
+        //@ts-ignore
+        ref.current.prepend(pDesc);
         //@ts-ignore
         refpDesc.current = pDesc;
       } else {
@@ -50,13 +60,6 @@ export const A11ySection: React.FC<Props> = ({
       }
     }
   }, [description, label]);
-
-  if (ref.current === null) {
-    //@ts-ignore
-    target.appendChild(el);
-    //@ts-ignore
-    ref.current = el;
-  }
 
   return (
     <>
