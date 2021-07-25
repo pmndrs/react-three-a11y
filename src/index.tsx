09a0e269adbb36af4a7ca0bd98f5aee7189f3d02
Alaric Baraou <alaric.baraou@gmail.com>
2021-07-25 18:44:41 +0200

    remove deprecated components

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-export * from './A11y';
+// export * from './A11y';
 export * from './A11yBind';
 export * from './A11yUserPreferences';
 export * from './A11yAnnouncer';
 export * from './A11yDebuger';
-export { A11ySection } from './A11ySection';
-export { A11yTag } from './A11yTag';
+// export { A11ySection } from './A11ySection';
+// export { A11yTag } from './A11yTag';
